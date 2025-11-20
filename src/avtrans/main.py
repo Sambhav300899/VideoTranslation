10cca19afc990b97c9446af7063260ee1221782c
sambhav <sambhav300899@gmail.com>
2025-11-19 20:13:37 -0500

    added comments and basic cleanup

--- src/avtrans/main.py
+++ src/avtrans/main.py
@@ -8,6 +8,7 @@ import librosa
 
 
 if __name__ == "__main__":
+    ## all paths
     video_path = pathlib.Path("../../data/Tanzania-2.mp4")
     srt_path = pathlib.Path("../../data/Tanzania-caption.srt")
     extracted_audio_path = video_path.with_name(
@@ -16,37 +17,44 @@ if __name__ == "__main__":
     translated_audio_path = video_path.with_name(video_path.stem + "_translated.wav")
     translated_text_path = video_path.parent / "translated_text.txt"
     final_video_path = video_path.with_name(video_path.stem + "_translated.mp4")
+    chunk_paths = pathlib.Path("../../data/chunks")
 
+    ## define our cloning and translation functions, add more models later
+    voice_clone_func = voice_clone.xtts_generate_from_transcript
+    translate_func = translate.helsinki_translate
+
+    ## create dirs
+    chunk_paths.mkdir(exist_ok=True)
+
+    ## extract audio
     utils.strip_audio_from_vid(video_path, extracted_audio_path)
 
+    ## get sentence groups, contains start, end, text
     sentence_groups = utils.get_sentence_groups(srt_path)
-
     untranslated_batch = [group["text"] for group in sentence_groups]
-    translated = translate.helsinki_translate(untranslated_batch)
+
+    ## translate and add to sentence groups
+    translated = translate_func(untranslated_batch)
     for i in range(len(sentence_groups)):
         sentence_groups[i]["translated_text"] = translated[i]
 
     torch.cuda.empty_cache()
 
-    chunk_paths = pathlib.Path("../../data/chunks")
-    chunk_paths.mkdir(exist_ok=True)
-
+    ## generate audio for each sentence group
     audio_paths = []
 
     for i, group in enumerate(tqdm.tqdm(sentence_groups)):
         chunk_audio_path = chunk_paths / f"{i}.wav"
 
         if not chunk_audio_path.exists():
-            voice_clone.xtts_generate_from_transcript(
+            voice_clone_func(
                 group["translated_text"], extracted_audio_path, chunk_audio_path
             )
 
         audio_paths.append(chunk_audio_path)
 
-    combined_audio_data = []
+    ## stretch audio to match sentence duration
     stretched_audio_paths = []
-
-    sample_rate = None
 
     abs_duration_diff = 0
     for i, audio_path in tqdm.tqdm(enumerate(audio_paths)):
@@ -60,6 +68,7 @@ if __name__ == "__main__":
         stretched_audio_paths.append(chunk_paths / f"{i}_stretched.wav")
         abs_duration_diff += abs(actual_duration - current_duration)
 
+    ## stitch all audio and add to video
     utils.stitch_audio(stretched_audio_paths, translated_audio_path)
     utils.combine_video_audio(video_path, translated_audio_path, final_video_path)
 
